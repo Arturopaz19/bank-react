994cc18456f83bfca6073a2a52264a21c00db78a
Arturo Paz <arturopaz.1910@gmail.com>
2020-11-17 22:39:30 -0600

    Se dividen componentes en Layout.

---
+++ src/components/layout/Menu.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Nav, Navbar } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+
+export default function Menu () {
+    console.log(window.location.pathname)
+    return (
+        <Navbar bg="light" expand="lg">
+            <Navbar.Brand href={'/'}>Sistema Bancario</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Item className={window.location.pathname === '/' ? 'nav-current' : ''}>
+                        <Link to={'/'}>Inicio</Link>
+                    </Nav.Item>
+                    <Nav.Item className={window.location.pathname === '/banks' ? 'nav-current' : ''}>
+                        <Link to={'/banks'}>Bancos</Link>
+                    </Nav.Item>
+                    <Nav.Item className={window.location.pathname === '/branches' ? 'nav-current' : ''}>
+                        <Link to={'/branches'}>Sucursales</Link>
+                    </Nav.Item>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    )
+}
--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,28 +1,12 @@
 import React from 'react'
-import { Nav, Navbar, Container } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+import Menu from './Menu'
 
 export default function Layout (props) {
     const { children } = props
     return (
         <div>
-            <Navbar bg="light" expand="lg">
-                <Navbar.Brand href={'/'}>Sistema Bancario</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
-                        <Nav.Item>
-                            <Link to={'/'}>Inicio</Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Link to={'/banks'}>Bancos</Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Link to={'/branches'}>Sucursales</Link>
-                        </Nav.Item>
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
+            <Menu />
             <Container>
                 {children}
             </Container>
